8a9422a0f38dc34977e9bc4cb5c3bf3197136b22
Yohan Alahakoon <yohankoon@gmail.com>
2023-11-09 09:16:07 +0530

    Completed Checkout UI

--- app/screens/CheckoutScreen.tsx
+++ app/screens/CheckoutScreen.tsx
@@ -1,33 +1,83 @@
-import { ViewStyle, View } from 'react-native'
-import React from 'react'
+import { ViewStyle, View, TextStyle } from 'react-native'
+import React, { useMemo, useState } from 'react'
+import { RadioGroup, RadioButtonProps } from 'react-native-radio-buttons-group'
 import { Screen, Spacer, Button, Text } from '../components'
 import { spacing } from '../theme'
+import { useReduxSelector } from '../redux'
 
 export const CheckoutScreen = () => {
-  return (
-    <Screen preset='auto' contentContainerStyle={$root}>
-      <View style={$checkoutContainer}>
-        <Text tx="checkoutScreen:total" preset='h3' />
-      </View>
-      <Spacer mainAxisSize={spacing.md} />
-      <Button
-        style={$checkoutButton}
-        tx="checkoutScreen:checkout"
-        onPress={() => {}}
-      />
-      <Spacer mainAxisSize={spacing.md} />
-    </Screen>
-  )
+    const { cartItems } = useReduxSelector(state => state.cart)
+
+    const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<string | undefined>('1');
+
+    const total = useMemo(() => {
+        return cartItems.reduce((acc, item) => {
+            return acc + item.price * item.quantity
+        }, 0)
+    }, [cartItems])
+
+    const paymentMethods : RadioButtonProps[] = useMemo(() => ([
+        {
+            id: '1',
+            label: 'Cash on Delivery',
+            value: 'cashOnDelivery'
+        },
+        {
+            id: '2',
+            label: 'Visa/Mastercard',
+            value: 'card'
+        }
+    ]), []);
+
+    return (
+        <Screen preset='auto' contentContainerStyle={$root}>
+            <View style={$checkoutContainer}>
+                <Text tx="checkoutScreen:total" preset='h4' weight='medium' />
+                <Spacer mainAxisSize={spacing.xxl} />
+                <Text text={`$ ${total}`} preset='h1' weight='medium' style={$total} />
+                <View style={$paymentMethodContainer}>
+                    <Text tx="checkoutScreen:paymentMethod" preset='h4' weight='medium' />
+                    <Spacer mainAxisSize={spacing.xl} />
+                    <RadioGroup
+                        radioButtons={paymentMethods}
+                        onPress={setSelectedPaymentMethod}
+                        selectedId={selectedPaymentMethod}
+                        containerStyle={$paymentMethod}
+                    />
+                </View>
+            </View>
+            <Spacer mainAxisSize={spacing.md} />
+            <Button
+                style={$checkoutButton}
+                tx="checkoutScreen:checkout"
+                onPress={() => { }}
+            />
+            <Spacer mainAxisSize={spacing.md} />
+        </Screen>
+    )
 }
 
-const $root : ViewStyle = {
+const $root: ViewStyle = {
     flex: 1,
+    paddingHorizontal: spacing.md,
+    marginTop: spacing.xxl,
 }
 
-const $checkoutContainer : ViewStyle = {
+const $checkoutContainer: ViewStyle = {
     flexGrow: 1,
 }
 
-const $checkoutButton : ViewStyle = {
-    marginHorizontal: spacing.md,
+const $total: TextStyle = {
+    alignSelf: 'center',
 }
+
+const $checkoutButton: ViewStyle = {
+
+}
+
+const $paymentMethodContainer: ViewStyle = {
+    marginTop: spacing.xxl,
+    alignItems: 'flex-start'
+}
+
+const $paymentMethod: ViewStyle = { marginLeft: spacing.xxxl }
